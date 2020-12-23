72f5b078bbb9c1ba50ea9d873cb21f4b4679b44c
Jeff-Owens Iyalekhue <j.oiyalekhue@hotmail.de>
2020-12-23 16:10:57 +0100

    quit and return functions added

---
+++ Assets/Scripts/QuitScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+    public void QuitGame()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+}
