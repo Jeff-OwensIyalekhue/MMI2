0e67e8d6a7f24ecbe7d84bec948ea8d18cc77a29
Ivo <ivo.christ97@gmail.com>
2021-01-05 17:38:26 +0100

    spawned balls by script

---
+++ Assets/Scripts/BallsInstantiation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallsInstantiation : MonoBehaviour
+{
+       // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {   
+        int count = 10;
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        for (int i = 0; i < count; i++)
+        {
+            int x = Random.Range(-1,1);
+            int z = Random.Range(-1,1);
+            int height = Random.Range(5,15);
+            Instantiate(myPrefab, new Vector3(x, height, z), Quaternion.identity);
+        }
+        
+    }
+}
