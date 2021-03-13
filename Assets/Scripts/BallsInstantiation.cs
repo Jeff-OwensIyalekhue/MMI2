4bfaf492a9e7f9814974fd108d689ab0cb15555b
Ivo <ivo.christ97@gmail.com>
2021-03-13 11:22:40 +0100

    not working ball destroying

--- Assets/Scripts/BallsInstantiation.cs
+++ Assets/Scripts/BallsInstantiation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class BallsInstantiation : MonoBehaviour
 {
@@ -15,23 +16,20 @@ public class BallsInstantiation : MonoBehaviour
 
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject[] myPrefabs;
-    
-    //public string[] prefabNames = {"Big String Ball.prefab", "Grey Fur Ball.prefab", "Leopard Fur Ball.prefab", "Wooden Ball.prefab", "Yellow Fabric Ball.prefab"};
-    //public GameObject[] prefabs;
+    public GameObject[] balls;
 
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
-        // load prefabs
-        //this.prefabs = loadPrefabs();
-
+        this.balls = new GameObject[this.myPrefabs.Length];
         this.amountBalls = Random.Range(5,20);
+        GameObject ballsContainer = GameObject.Find("BallsContainer").transform;
+
         // Instantiate at position (0, 0, 0) and zero rotation.
         for (int i = 0; i < this.amountBalls; i++)
         {
             // pick random prefab
             int index = Random.Range(0, this.myPrefabs.Length - 1);
-            print(index);
             GameObject prefab = this.myPrefabs[index];
 
             // render at random position
@@ -40,8 +38,23 @@ public class BallsInstantiation : MonoBehaviour
             float height = Random.Range(minY, maxY);
             GameObject ball = Instantiate(prefab, transform.position + new Vector3(x, height, z), Quaternion.identity);
             Rigidbody rb = ball.GetComponent<Rigidbody>();
+            ball.transform.parent = ballsContainer;
             rb.AddForce(Random.onUnitSphere * 10, ForceMode.Impulse);
+            this.balls[i] = ball;
         }
 
+        // destroy balls after end of session
+        Task.Delay(1000).ContinueWith(t => destroyBalls(ballsContainer));
+    }
+
+    void destroyBalls(GameObject obj)
+    {
+        /*
+        foreach(GameObject ball in this.balls)
+            {
+                Destroy(ball);
+            }
+            */
+        Destroy(obj);
     }
 }
